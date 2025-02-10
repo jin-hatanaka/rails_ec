53804220fad8043407a200ef3adf77a3b4b77a5d
jin-hatanaka <ringo.lemon15@gmail.com>
2025-02-10 16:43:31 +0900

    N+1問題の修正。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,11 @@
 
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.with_attached_image
   end
 
   def show
     @product = Product.find(params[:id])
-    @related_products = Product.order(created_at: :desc)
+    @related_products = Product.order(created_at: :desc).with_attached_image
   end
 end
