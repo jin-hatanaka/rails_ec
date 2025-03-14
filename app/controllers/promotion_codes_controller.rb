280ae974c3b9df8ded2a815dc730835d66f58991
jin-hatanaka <ringo.lemon15@gmail.com>
2025-03-14 16:13:21 +0900

    プロモーションコード機能の修正(1)、レイアウト微修正

--- app/controllers/promotion_codes_controller.rb
+++ app/controllers/promotion_codes_controller.rb
@@ -2,13 +2,9 @@
 
 class PromotionCodesController < ApplicationController
   def create
-    if session[:promotion_code]
-      redirect_to request.referer, alert: '適用できるプロモーションコードは1つまでです。'
-    elsif PromotionCode.find_by(code: params[:code]).nil?
-      redirect_to request.referer, alert: '無効なプロモーションコードです。'
-    else
+    redirect_to request.referer, alert: '適用できるプロモーションコードは1つまでです。' and return if session[:promotion_code]
+    redirect_to request.referer, alert: '無効なプロモーションコードです。' and return if PromotionCode.find_by(code: params[:code]).nil?
       session[:promotion_code] = PromotionCode.find_by(code: params[:code]).code
       redirect_to request.referer, notice: 'プロモーションコードが適用されました。'
-    end
   end
 end
